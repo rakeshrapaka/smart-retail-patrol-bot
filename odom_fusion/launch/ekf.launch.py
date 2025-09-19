b4db7dca88ef851bca791c921dc9a0ea58d563a7
sanket369 <skhade369@gmail.com>
2025-09-19 10:01:26 +0000

    folder structure changed

--- odom_fusion/launch/ekf.launch.py
+++ odom_fusion/launch/ekf.launch.py
@@ -8,6 +8,6 @@ def generate_launch_description():
             executable='ekf_node',
             name='ekf_filter_node',
             output='screen',
-            parameters=['/home/smart_retail_bot/src/smart-retail-patrol-bot/odom_fusion/config/ekf.yaml']
+            parameters=['/home/smart_retail_bot/src/smart_retail_bot/odom_fusion/config/ekf.yaml']
         )
     ])
